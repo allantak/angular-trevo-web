0b3bdce5b9064a6630e982e70c2619386bd88858
Allan Takeuchi Bustamante <allanxd132@gmail.com>
2023-05-30 13:10:29 -0300

    landpage done

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -9,6 +9,8 @@ import { ButtonCategoryComponent } from './button-category/button-category.compo
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NewletterComponent } from './newletter/newletter.component';
 import { FormsModule } from '@angular/forms';
+import { VideoComponent } from './video/video.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
@@ -20,7 +22,9 @@ import { FormsModule } from '@angular/forms';
     CardComponent,
     CategoryComponent,
     ButtonCategoryComponent,
-    NewletterComponent
+    NewletterComponent,
+    VideoComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
@@ -31,7 +35,9 @@ import { FormsModule } from '@angular/forms';
     HeroComponent,
     ContrastComponent,
     CategoryComponent,
-    NewletterComponent
+    NewletterComponent,
+    VideoComponent,
+    FooterComponent
   ]
 })
 export class ComponentsModule { }
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+
+  imgFacebook: string ='assets/social-midia/facebook.png';
+  imgTwitter: string ='assets/social-midia/twitter.png';
+  imgInstragram: string ='assets/social-midia/instagram.png';
+  imgYoutube: string ='assets/social-midia/youtube.png';
+
+  imgLogoVerde: string ='assets/logo-verde.png';
+
+}
